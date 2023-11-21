874ce5090284b83e5b88ad548faba4790161decf
Andrey Korotkiy <Andreikorotkiy@gmail.com>
2023-11-22 00:48:36 +0300

    update

---
+++ House_Prices/input/src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='house_prices',
+    packages=find_packages(),
+    version='0.1.0',
+    description='https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques',
+    author='Andrey Korotkiy',
+)
